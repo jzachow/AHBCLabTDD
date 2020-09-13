74df1940f0eb499933c6a0b0deb0637dec111376
Justin <justin.zachow@gmail.com>
2020-09-13 12:16:00 -0400

    Add RockBeatsScissorsTest

--- ROSHAMBO/RPS.cs
+++ ROSHAMBO/RPS.cs
@@ -2,7 +2,14 @@
 
 namespace ROSHAMBO
 {
-    public class Class1
+    public class RPS
     {
+        public int PlayRound(string playerChoice, string gameChoice)
+        {
+            if (playerChoice.Equals("rock") && gameChoice.Equals("scissors"))
+                return 1;
+            else
+                return 0;
+        }
     }
 }
--- ROSHAMBOTest/RPSTest.cs
+++ ROSHAMBOTest/RPSTest.cs
@@ -1,14 +1,23 @@
+using ROSHAMBO;
 using System;
 using Xunit;
 
 namespace ROSHAMBOTest
 {
-    public class UnitTest1
+    public class RPSTest
     {
         [Fact]
-        public void Test1()
+        public void RockBeatsScissorsTest()
         {
+            //Arrange
+            var expectedResult = 1;
+            RPS rps = new RPS();
 
+            //Act
+            int result = rps.PlayRound("rock", "scissors");
+
+            //Assert
+            Assert.Equal(expectedResult, result);
         }
     }
 }
