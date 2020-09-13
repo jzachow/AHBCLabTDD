1b36667d2cb40cdbbab9c4ef160555dab2e02f64
Justin <justin.zachow@gmail.com>
2020-09-13 13:49:58 -0400

    Add RandomPlayerCanReturnPapertest

---
+++ ROSHAMBO/RandomPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ROSHAMBO
+{
+    public class RandomPlayer
+    {
+        public string GenerateChoice()
+        {
+            Random random = new Random();
+            int choice = random.Next(2);
+
+            switch (choice)
+            {
+                case 0:
+                    return "paper";                
+                default:                
+                    return "rock";
+            }           
+        }
+    }
+}
--- ROSHAMBOTest/RandomPlayerTest.cs
+++ ROSHAMBOTest/RandomPlayerTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ROSHAMBO;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -7,6 +8,40 @@ namespace ROSHAMBOTest
 {
     public class RandomPlayerTest
     {
-        
+        [Fact]
+        public void RandomPlayerCanReturnRockTest()
+        {
+            //Arrange
+            var expectedResult = "rock";
+            var randomPlayer = new RandomPlayer();
+            string result = string.Empty;
+
+            //Act
+            do
+            {
+                result = randomPlayer.GenerateChoice();
+            } while (!result.Equals("rock"));
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void RandomPlayerCanReturnPaperTest()
+        {
+            //Arrange
+            var expectedresult = "paper";
+            var randomPlayer = new RandomPlayer();
+            string result = string.Empty;
+
+            //Act
+            do
+            {
+                result = randomPlayer.GenerateChoice();
+            } while (!result.Equals("paper"));
+
+            //Assert
+            Assert.Equal(expectedresult, result);
+        }
     }
 }
