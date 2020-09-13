552c64efac3e527e85914d1f500190e0a8f5b3f6
Justin <justin.zachow@gmail.com>
2020-09-13 12:28:45 -0400

    Refactor RPS.PlayRound method

--- ROSHAMBO/RPS.cs
+++ ROSHAMBO/RPS.cs
@@ -6,16 +6,18 @@ namespace ROSHAMBO
     {
         public int PlayRound(string playerChoice, string gameChoice)
         {
-            if (playerChoice.Equals(gameChoice))
-                return 2;
-            if (playerChoice.Equals("rock") && gameChoice.Equals("scissors"))
-                return 1;
-            if (playerChoice.Equals("paper") && gameChoice.Equals("rock"))
-                return 1;
-            if (playerChoice.Equals("scissors") && gameChoice.Equals("paper"))
-                return 1;
+            int result = 0;
 
-            return 0;
+            if (playerChoice.Equals(gameChoice))
+                result = 2;
+            if (playerChoice.Equals("rock") && gameChoice.Equals("scissors"))
+                result = 1;
+            if (playerChoice.Equals("paper") && gameChoice.Equals("rock"))
+                result = 1;
+            if (playerChoice.Equals("scissors") && gameChoice.Equals("paper"))
+                result = 1;
+
+            return result;
         }
     }
 }
